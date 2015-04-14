a858134e98f7da3f3b24fbefa6f84a14930d2ed0
guilherme <guilherme.wd@live.com>
2015-04-14 14:15:49 -0300

    applied improvements

--- wp-content/themes/wedding/index.php
+++ wp-content/themes/wedding/index.php
@@ -16,15 +16,9 @@
 							<label for="">Nome como está no convite</label>
 							<input type="text">
 						</li>
-						<li class="field-select">
+						<li>
 							<label for="">Quantidade de pessoas</label>
-							<select name="" id="">
-								<option value="">1</option>
-								<option value="">2</option>
-								<option value="">3</option>
-								<option value="">4</option>
-								<option value="">5</option>
-							</select>
+							<input type="text">
 						</li>
 						<li class="field-radio">
 							<label for="">SIM <input type="radio"></label>
@@ -87,7 +81,10 @@
 				<div class="maps">
 					<div class="container">
 						<div class="box-info">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet nam minus consequuntur voluptas dolore inventore nemo! Enim, aliquid, obcaecati. Suscipit.
+							<div class="info">
+								<h2 class="title">title</h2>								
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet nam minus consequuntur voluptas dolore inventore nemo! Enim, aliquid, obcaecati. Suscipit.
+							</div>
 						</div>
 					</div>
 					<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d60403.05823789234!2d-41.934876750000015!3d-18.8785998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1scatedral+governador+valadares!5e0!3m2!1spt-BR!2sbr!4v1428922489870" height="450" frameborder="0"></iframe>
