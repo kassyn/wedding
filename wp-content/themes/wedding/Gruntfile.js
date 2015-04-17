64089bb0c4c8489a4ce7fad4b69a175aa7d004e1
kassyn <kassyntec@gmail.com>
2015-04-17 03:00:12 -0300

    include grunt css minify

--- wp-content/themes/wedding/Gruntfile.js
+++ wp-content/themes/wedding/Gruntfile.js
@@ -33,6 +33,14 @@ module.exports = function(grunt) {
 			}
     	},
 
+    	cssmin : {
+    		target : {
+		    	files : {
+		      		'style.css' : 'style.css'
+		    	}
+		  	}
+    	},
+
 		watch: {
 		    script : {
 		    	files : '<%= concat.site.src %>',
@@ -47,7 +55,9 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
 	grunt.loadNpmTasks( 'grunt-contrib-concat' );
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
 
 	grunt.registerTask( 'js', ['jshint', 'concat'] );
 	grunt.registerTask( 'jsmin', ['jshint', 'concat', 'uglify'] );
+	grunt.registerTask( 'deploy', ['jsmin', 'cssmin'] );
 };
